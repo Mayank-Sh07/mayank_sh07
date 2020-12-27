c6525b4c357e4fd78b52e359281e5546111272b8
Mayank-Sh07 <mike74600@gmail.com>
2020-12-27 22:24:40 +0530

    Added Jost Font CDN , replaced Roboto

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,8 @@ export default class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link
+            href='https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap'
             rel='stylesheet'
-            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
           />
         </Head>
         <body>
